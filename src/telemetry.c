0b2f67913c36d41dc9327d051bdd2914b73417d3
Matas Razgunas <matas.razgunas@gmail.com>
2016-09-18 15:07:36 +0300

    Fix data not being recieved bug

--- src/telemetry.c
+++ src/telemetry.c
@@ -9,7 +9,7 @@
 #include "bme280.h"
 #include "analog.h"
 
-#define SERIAL_DEVICE SDU1
+#define SERIAL_DEVICE SD1
 
 static virtual_timer_t led_vt; //Timer for rx
 position_t last_pos;
@@ -31,12 +31,13 @@ void blink() {
 /*
  * Mavlink receive
  */
-static THD_WORKING_AREA(waMavlinkThread, 2000);
+static THD_WORKING_AREA(waMavlinkThread, 5000);
 static THD_FUNCTION(MavlinkThread, arg) {
 
 	(void) arg;
 	static mavlink_message_t msg;
 	static mavlink_status_t status;
+	msg_t charData;
 
 	event_listener_t serialData;
 	eventflags_t flags;
@@ -45,19 +46,24 @@ static THD_FUNCTION(MavlinkThread, arg) {
 
 	chRegSetThreadName("mavlink");
 	while (true) {
-		chEvtWaitOneTimeout(EVENT_MASK(1), MS2ST(10));
+		chEvtWaitOneTimeout(EVENT_MASK(1), TIME_INFINITE);
 		flags = chEvtGetAndClearFlags(&serialData);
-		if(flags & CHN_INPUT_AVAILABLE)
-		{
-			msg_t charData;
-			do {
-				charData = chnGetTimeout(&SERIAL_DEVICE, TIME_IMMEDIATE);
-				blink(); //Blink led for received data
-				if (mavlink_parse_char(MAVLINK_COMM_0, charData, &msg, &status)) {
-					handle_mavlink_message(msg);
-				}
-			} while(charData != Q_TIMEOUT);
+		if(flags & CHN_INPUT_AVAILABLE) {
+		    //blink();
 		}
+        do {
+            charData = chnGetTimeout(&SERIAL_DEVICE, TIME_IMMEDIATE);
+            if(charData > 0xFF || charData == Q_TIMEOUT) {
+                break;
+            }
+            if (mavlink_parse_char(MAVLINK_COMM_0, (uint8_t)charData, &msg, &status)) {
+                handle_mavlink_message(msg);
+                blink(); //Blink led for received packet
+            }
+        } while(charData != Q_TIMEOUT);
+        if(flags & SD_OVERRUN_ERROR) {
+            blink();
+        }
 	}
 	/* This point may be reached if shut down is requested. */
 }
@@ -66,6 +72,16 @@ static THD_WORKING_AREA(waMavlinkTx, 1000);
 static THD_FUNCTION(MavlinkTx, arg) {
     (void) arg;
 
+    // Define the system type, in this case an airplane
+    uint8_t system_type = MAV_TYPE_GIMBAL;
+    uint8_t autopilot_type = MAV_AUTOPILOT_INVALID;
+
+    uint8_t system_mode = MAV_MODE_AUTO_ARMED; ///< Booting up
+    uint32_t custom_mode = 0;   ///< Custom mode, can be defined by user/adopter
+    uint8_t system_state = MAV_STATE_ACTIVE; ///< System ready for flight
+
+
+    mavlink_msg_heartbeat_send(MAVLINK_COMM_0, system_type, autopilot_type, system_mode, custom_mode, system_state);
 
 	while(true) {
         float temp = get_tempeture();
@@ -76,14 +92,7 @@ static THD_FUNCTION(MavlinkTx, arg) {
         get_analog_sensor_values(&co_gas, &no2_gas, &nh3_gas, &no_gas, &so2_gas);
 
         mavlink_msg_gas_sensor_board_send(MAVLINK_COMM_0, hum, temp, baro, nh3_gas, no2_gas, co_gas, no_gas, so2_gas);
-
-        // Define the system type, in this case an airplane
-        uint8_t system_type = MAV_TYPE_GENERIC;
-        uint8_t autopilot_type = MAV_AUTOPILOT_INVALID;
-
-        uint8_t system_mode = MAV_MODE_AUTO_ARMED; ///< Booting up
-        uint32_t custom_mode = 0;   ///< Custom mode, can be defined by user/adopter
-        uint8_t system_state = MAV_STATE_ACTIVE; ///< System ready for flight
+        mavlink_msg_global_position_int_send(MAVLINK_COMM_0, ST2MS(chVTGetSystemTime()), last_pos.lat, last_pos.lon, last_pos.alt, last_pos.r_alt, 0, 0, 0, 0);
 
         // Pack the message
         mavlink_msg_heartbeat_send(MAVLINK_COMM_0, system_type, autopilot_type, system_mode, custom_mode, system_state);
@@ -101,10 +110,13 @@ void handle_mavlink_message(mavlink_message_t msg) {
 			break;
 		}
 		case MAVLINK_MSG_ID_PARAM_REQUEST_LIST: {
-			mavlink_msg_param_value_send(MAVLINK_COMM_0, "TEST1\0", 1.0f, MAV_PARAM_TYPE_UINT8, 3, 0);
-			mavlink_msg_param_value_send(MAVLINK_COMM_0, "TEST2\0", 1.0f, MAV_PARAM_TYPE_UINT8, 3, 1);
-			mavlink_msg_param_value_send(MAVLINK_COMM_0, "TEST3\0", 1.0f, MAV_PARAM_TYPE_UINT8, 3, 2);
+			mavlink_msg_param_value_send(MAVLINK_COMM_0, "TEST1\0", 1.0f, MAV_PARAM_TYPE_UINT8, 1, 0);
+			mavlink_msg_param_value_send(MAVLINK_COMM_0, "TEST2\0", 1.0f, MAV_PARAM_TYPE_UINT8, 1, 1);
+			mavlink_msg_param_value_send(MAVLINK_COMM_0, "TEST3\0", 1.0f, MAV_PARAM_TYPE_UINT8, 1, 2);
 			break;
+		}
+		case MAVLINK_MSG_ID_GAS_SENSOR_BOARD: {
+		    break;
 		}
 		case MAVLINK_MSG_ID_GLOBAL_POSITION_INT: {
 		    mavlink_global_position_int_t decode;
@@ -112,6 +124,7 @@ void handle_mavlink_message(mavlink_message_t msg) {
 		    last_pos.lat = decode.lat;
 		    last_pos.lon = decode.lon;
 		    last_pos.alt = decode.alt;
+		    last_pos.r_alt = decode.relative_alt;
 		    break;
 		}
 		case MAVLINK_MSG_ID_DATA_STREAM: {
@@ -136,7 +149,7 @@ void init_telemetry() {
 	palClearPad(GPIOB, GPIOB_STATUS_LED);
 	chVTObjectInit(&led_vt);
 
-	chThdCreateStatic(waMavlinkThread, sizeof(waMavlinkThread), NORMALPRIO + 1,
+	chThdCreateStatic(waMavlinkThread, sizeof(waMavlinkThread), NORMALPRIO + 2,
 			MavlinkThread, NULL);
 	chThdCreateStatic(waMavlinkTx, sizeof(waMavlinkTx), NORMALPRIO + 1,
 	            MavlinkTx, NULL);
--- src/telemetry.h
+++ src/telemetry.h
@@ -5,6 +5,7 @@ typedef struct position_t {
     int32_t lat; /* Latitude, expressed as degrees * 1E7 */
     int32_t lon; /* Longitude, expressed as degrees * 1E7 */
     int32_t alt; /* Altitude in meters, expressed as * 1000 (millimeters), AMSL */
+    int32_t r_alt;
 } position_t;
 
 void init_telemetry(void);
