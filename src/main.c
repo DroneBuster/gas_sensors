43f0949c4eb9634ba4c503c9d6a0cea8a8281fc7
Matas Razgunas <matas.razgunas@gmail.com>
2016-09-18 15:10:15 +0300

    Code cleanup

--- src/main.c
+++ src/main.c
@@ -1,19 +1,3 @@
-/*
- ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
 #include "ch.h"
 #include "hal.h"
 
@@ -22,8 +6,6 @@
 #include "chprintf.h"
 #include "telemetry.h"
 #include "analog.h"
-//#include "logging.h"
-
 
 static const I2CConfig i2cconfig = { OPMODE_I2C, 100000, //100kHz
         STD_DUTY_CYCLE };
@@ -49,7 +31,6 @@ int main(void) {
     i2cStart(&I2CD1, &i2cconfig);
     init_bme280();
 
-    //Initialize USB serial
     /*
      * Initializes a serial-over-USB CDC driver.
      */
@@ -61,18 +42,11 @@ int main(void) {
      * Note, a delay is inserted in order to not have to disconnect the cable
      * after a reset.
      */
-    //usbDisconnectBus(serusbcfg.usbp);
     chThdSleepMilliseconds(1500);
     usbStart(serusbcfg.usbp, &usbcfg);
-    // usbConnectBus(serusbcfg.usbp);
 
-    /*chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO + 1, Thread1,
-            NULL);
-*/
     init_analog();
     measure_sensors();
-    //init_logging();
-    //write_to_file();
     init_telemetry(); //Init last to have all mtx objects init
 
     /*
